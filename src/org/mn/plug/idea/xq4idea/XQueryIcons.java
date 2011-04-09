2d8b6bd2f7471425de0131952297ec3ad59d244e
Matt Nathan <matt.nathan+github@gmail.com>
2011-04-09 12:06:01 +0100

    Shifted around the resources a bit and added a file icon for XQ

---
+++ src/org/mn/plug/idea/xq4idea/XQueryIcons.java
@@ -0,0 +1,14 @@
+package org.mn.plug.idea.xq4idea;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Collection of icons used in this plugin
+ *
+ * @author Matt Nathan
+ */
+public interface XQueryIcons {
+  Icon XQUERY_ICON_16x16 = IconLoader.getIcon("/icons/fileIcon-16x16.png");
+}
